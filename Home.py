1de7b2e21a3602a234f817f9aa92a46397cfe32e
Joshua <88333204+Oaky112@users.noreply.github.com>
2024-03-22 01:37:47 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -150,7 +150,7 @@ def main():
         background-image: url('https://mrwallpaper.com/images/hd/download-firewatch-wallpaper-ervaypm7906b8op2.jpg'); 
         background-size: 140% auto; /* Change size as needed *
         background-repeat: no-repeat; /* Adjust as needed */
-        background-position: center -50%; /* Adjust as needed */
+        background-position: center -55%; /* Adjust as needed */
     }
     </style>
     """
