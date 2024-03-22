3481b2df57a46d2b02399c2b8cb642bb2aa520be
Joshua <88333204+Oaky112@users.noreply.github.com>
2024-03-22 01:10:21 +0000

    Updating files

--- Home.py
+++ Home.py
@@ -148,9 +148,9 @@ def main():
         font-family: -apple-system, BlinkMacSystemFont, sans-serif;
         overflow: auto;
         background-image: url('https://mrwallpaper.com/images/hd/download-firewatch-wallpaper-ervaypm7906b8op2.jpg'); 
-        background-size: 80% auto; /* Change size as needed *
+        background-size: cover; /* Change size as needed *
         background-repeat: no-repeat; /* Adjust as needed */
-        background-position: center 30%; /* Adjust as needed */
+        background-position: center bottom; /* Adjust as needed */
     }
     </style>
     """
